5b6bf35899c6a357d03f97c0e2c8b24e5778aeaa
Craig O'Neil <kbb08124@uni.strath.ac.uk>
2015-01-30 12:23:15 +0000

    Initial Application uploaded to BitBucket, currently reads Gyroscope values and displays them on the screen

---
+++ GyroScope/GyroScope/bin/Android/Debug/CordovaApp-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\Users\Craig\Documents\Visual Studio 2013\Projects\GyroScope\GyroScope\bld\Debug\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
+C:\Users\Craig\Documents\Visual Studio 2013\Projects\GyroScope\GyroScope\bld\Debug\platforms\android\ant-build\CordovaApp.ap_ \
+C:\Users\Craig\Documents\Visual Studio 2013\Projects\GyroScope\GyroScope\bld\Debug\platforms\android\ant-build\classes.dex \
+C:\Users\Craig\Documents\Visual Studio 2013\Projects\GyroScope\GyroScope\bld\Debug\platforms\android\CordovaLib\ant-build\classes.jar \
---
+++ GyroScope/GyroScope/bld/Debug/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Craig\Documents\Visual Studio 2013\Projects\GyroScope\GyroScope\bld\Debug\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Users\Craig\Documents\Visual Studio 2013\Projects\GyroScope\GyroScope\bld\Debug\platforms\android\CordovaLib\AndroidManifest.xml \
