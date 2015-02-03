dd0428b8cebc8fa6ef0cdf4d335e6d274984f9b0
Craig O'Neil <kbb08124@uni.strath.ac.uk>
2015-02-03 11:13:51 +0000

    Set up JS to read values of motion 10 times a second, and display the average of each 0.5 second on screen

--- GyroScope/GyroScope/scripts/motion.js
+++ GyroScope/GyroScope/scripts/motion.js
@@ -3,6 +3,10 @@ var dataContainerMotion;
 var dataContainerCount;
 var counter;
 
+var xArray = [];
+var yArray = [];
+var zArray = [];
+
 function init() {
     dataContainerOrientation = document.getElementById('dataContainerOrientation');
     dataContainerMotion = document.getElementById('dataContainerMotion');
@@ -13,10 +17,11 @@ function init() {
 function orient(){
     if (window.DeviceOrientationEvent) {
         window.addEventListener('deviceorientation', function (event) {
-            updateCount();
             var alpha = (Math.round(event.alpha * 100) / 100).toFixed(2);
             var beta = (Math.round(event.beta * 100) / 100).toFixed(2);
             var gamma = (Math.round(event.gamma * 100) / 100).toFixed(2);
+
+            event.target.removeEventListener(event.type, arguments.callee);
 
             if (alpha != null || beta != null || gamma != null)
                 dataContainerOrientation.innerHTML = 'alpha: ' + alpha + '<br/>beta: ' + beta + '<br/>gamma: ' + gamma;
@@ -29,16 +34,18 @@ function motion(){
     if (window.DeviceMotionEvent) {
         window.addEventListener('devicemotion', function (event) {
             var x = (Math.round(event.acceleration.x * 100) / 100).toFixed(2);
+            xArray.push(x);
             var y = (Math.round(event.acceleration.y * 100) / 100).toFixed(2);
+            yArray.push(y);
             var z = (Math.round(event.acceleration.z * 100) / 100).toFixed(2);
+            zArray.push(z);
             var r = event.rotationRate;
 
-            var html = 'Acceleration:<br />';
-            html += 'x: ' + x + '<br />y: ' + y + '<br/>z: ' + z + '<br />';
-            html += 'Rotation rate:<br />';
-            if (r != null) html += 'alpha: ' + (Math.round(r.alpha * 100) / 100).toFixed(2) + '<br />beta: ' + (Math.round(r.beta * 100) / 100).toFixed(2) + '<br/>gamma: ' + (Math.round(r.gamma * 100) / 100).toFixed(2) + '<br />';
-            dataContainerMotion.innerHTML = html;
-            updateCount();
+            event.target.removeEventListener(event.type, arguments.callee);
+
+
+            //if (r != null) html += 'alpha: ' + (Math.round(r.alpha * 100) / 100).toFixed(2) + '<br />beta: ' + (Math.round(r.beta * 100) / 100).toFixed(2) + '<br/>gamma: ' + (Math.round(r.gamma * 100) / 100).toFixed(2) + '<br />';
+            
         });
     }
 }
@@ -46,4 +53,27 @@ function motion(){
 function updateCount() {
   counter++;
     dataContainerCount.innerHTML = counter;
+}
+
+function getReading() {
+    window.setInterval(function () {
+        orient();
+        motion();
+        updateCount();
+        if (counter % 5 == 0) {
+            var xSum = 0;
+            var ySum = 0;
+            var zSum = 0;
+            var arrLength = xArray.length;
+            while (xArray.length > 0) {
+                xSum += parseFloat(xArray.pop());
+                ySum += parseFloat(yArray.pop());
+                zSum += parseFloat(zArray.pop());
+            }
+            var html = 'Acceleration:<br />';
+            html += 'x: ' + (Math.round(xSum / arrLength * 100) / 100).toFixed(2) + '<br />y: ' + (Math.round(ySum / arrLength * 100) / 100).toFixed(2) + '<br/>z: ' + (Math.round(zSum / arrLength * 100) / 100).toFixed(2) + '<br />';
+            //html += 'Rotation rate:<br />';
+            dataContainerMotion.innerHTML = html;
+        }
+    }, 100);
 }
