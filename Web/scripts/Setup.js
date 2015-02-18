3081d6c35884aadbb6a82747b371c6bdb9202275
Craig O'Neil <kbb08124@uni.strath.ac.uk>
2015-02-18 14:49:51 +0000

    Added setup script to get screen dimensions and create button to start program

---
+++ Web/scripts/Setup.js
@@ -0,0 +1,24 @@
+﻿var width = screen.availHeight;
+
+var height = screen.availWidth;
+
+var screenRatio;
+
+var realWidth;
+
+var realHeight;
+
+
+
+function setValues() {
+        if (window.innerHeight > window.innerWidth) { realWidth = window.innerHeight; realHeight = window.innerWidth; screenRatio = (window.innerWidth / window.innerHeight); }
+
+        else { realWidth = window.innerWidth; realHeight = window.innerHeight; screenRatio = (window.innerHeight / window.innerWidth); }
+}
+
+function getDimensions() {
+    setValues();
+    var myElement = document.getElementById("gobutton");
+    myElement.style.height = 0.96* realHeight + "px";
+    myElement.style.width = 0.97* realWidth + "px";
+}
