2f18fe4175fe62e51e5db6eb5b460e51cb8b3105
Craig O'Neil <kbb08124@uni.strath.ac.uk>
2015-02-19 14:33:48 +0000

    Added AngularJS locally to enable faster loading times Set-up start button to disappear when clicked and calibration page to appear Started calibration page Added Progress bar to calibration page

---
+++ Web/scripts/balanceController.js
@@ -0,0 +1,28 @@
+﻿var calibTime = 0;
+
+function balanceController($scope) {
+    $scope.hideStart = true;
+    $scope.toggleStart = function () {
+        $scope.hideStart = !$scope.hideStart;
+        startCalibration();
+    }
+}
+
+function startCalibration() {
+    calibTime = 0;
+    var calib = setInterval(function () {
+        calibTime+=0.1;
+        document.getElementById('progress').style.width = Math.ceil((calibTime * 1000) / 300) + "%";
+        showTime();
+        if (calibTime > 30.1) {
+            document.getElementById('progress').classList.remove("active");
+            clearInterval(calib);
+            ($scope.progress).removeClass('active');
+        }
+    }, 100);
+}
+
+function showTime() {
+    var t = "Time elapsed is: " + Math.round(calibTime) + "s";
+    document.getElementById("time").innerHTML = t;
+}
