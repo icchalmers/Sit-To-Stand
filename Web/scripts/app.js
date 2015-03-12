5e5123cf55ad85d7b8025fc3419d2f5fb5dee31b
Craig O'Neil <kbb08124@uni.strath.ac.uk>
2015-03-12 21:36:27 +0000

    Removed sampling rate to record all orientation events Averaging over 0.5s - provides much smoother results although slight lag Fixed JSLint errors Removed unused script files

--- Web/scripts/app.js
+++ Web/scripts/app.js
@@ -1,4 +1,4 @@
-﻿var calibTime = 0;
+var calibTime = 0;
 var width = 0;
 var height = 0;
 
@@ -8,7 +8,7 @@ var realHeight = 500;
 var audio;
 
 var screenNum = 1;
-var angleCalibrations = [0,0,0];
+var angleCalibrations = [0, 0, 0];
 
 var alpha = [];
 var beta = [];
@@ -35,39 +35,42 @@ var message = "";
 
 //Helper methods for controller to use
 function setValues() {
+    "use strict";
     width = screen.availWidth;
     height = screen.availHeight;
 
     if (height > width) {
         realWidth = height;
         realHeight = width;
-    }
-    else {
+    } else {
         realWidth = width;
         realHeight = height;
     }
 }
 
 function showTime() {
+    "use strict";
     var t = "Time elapsed is: " + Math.floor(calibTime / 10) + "s\n";
     document.getElementById("time").innerHTML = t;
 }
 
 function playSound() {
+    "use strict";
     audio.play();
 }
 
 function updateBar() {
+    "use strict";
     calibTime += 0.1;
     document.getElementById('progress').style.width = Math.ceil(calibTime * 3.33333334) + "%";
     showTime();
 }
 
 function orient() {
-    
+    "use strict";
     var handler = function (event) {
         if (event !== null) {
-            if (event.alpha != null && event.alpha !== 0) {
+            if (event.alpha !== null && event.alpha !== 0) {
                 if (smoothedAlpha === 0) {
                     smoothedAlpha = event.alpha;
                 }
@@ -75,7 +78,7 @@ function orient() {
                 alpha.push(smoothedAlpha);
                 message += "A:" + smoothedAlpha;
             }
-            if (event.beta != null && event.beta !== 0) {
+            if (event.beta !== null && event.beta !== 0) {
                 if (smoothedBeta === 0) {
                     smoothedBeta = event.beta;
                 }
@@ -83,7 +86,7 @@ function orient() {
                 beta.push(smoothedBeta);
                 message += " B:" + smoothedBeta;
             }
-            if (event.gamma != null && event.gamma !== 0) {
+            if (event.gamma !== null && event.gamma !== 0) {
                 if (smoothedGamma === 0) {
                     smoothedGamma = event.gamma;
                 }
@@ -96,10 +99,9 @@ function orient() {
             var mins = date.getMinutes().toString();
             var secs = date.getSeconds().toString();
             var mil = date.getMilliseconds().toString();
-            message += (" T:" + hours +":"+mins+":"+secs+":"+mil+ "\n");
+            message += (" T:" + hours + ":" + mins + ":" + secs + ":" + mil + "\n");
         }
-        window.removeEventListener('deviceorientation', handler, false);
-        listen = null;
+        //window.removeEventListener('deviceorientation', handler, false);
     };
 
 
@@ -109,15 +111,16 @@ function orient() {
 }
 
 function convert(type, angle) {
-    if (type == 'deg') {
+    "use strict";
+    if (type === 'deg') {
         return angle * (180 / Math.PI);
-    }
-    else {
+    } else {
         return angle * (Math.PI / 180);
     }
 }
 
 function smoothAngles(calibrating) {
+    "use strict";
     var tempA = alpha;
     var tempB = beta;
     var tempG = gamma;
@@ -151,20 +154,19 @@ function smoothAngles(calibrating) {
         gSin += Math.sin(convert("rad", parseFloat(tempG.pop())));
     }
 
-    var a = (360 + convert("deg", Math.atan2(aSin, aCos)))%360;
+    var a = (360 + convert("deg", Math.atan2(aSin, aCos))) % 360;
     var b = convert("deg", Math.atan2(bSin, bCos));
     var g = convert("deg", Math.atan2(gSin, gCos));
 
     //message += "Averaged:\nA:" + a + " B:" + b + " G:" + g + "\n";
 
-    if (aLength * bLength * gLength != 0) {
+    if (aLength * bLength * gLength !== 0) {
         angleCalibrations = [a, b, g];
         if (calibrating) {
             totalAlpha.push(a);
             totalBeta.push(b);
             totalGamma.push(g);
-        }
-        else {
+        } else {
             diffAlpha = convert("deg", Math.atan2(Math.sin(convert("rad", angleCalibrations[0] - calibAlpha)), Math.cos(convert("rad", angleCalibrations[0] - calibAlpha))));
             diffBeta = angleCalibrations[1] - calibBeta;
             diffGamma = angleCalibrations[2] - calibGamma;
@@ -173,11 +175,13 @@ function smoothAngles(calibrating) {
 }
 
 function setSource() {
+    "use strict";
     audio = document.getElementById('player');
     audio.src = 'sounds/startTone.mp3';
 }
 
 function requiresUserGesture() {
+    "use strict";
     // test if play() is ignored when not called from an input event handler
     var test = document.createElement('audio');
     test.play();
@@ -185,6 +189,7 @@ function requiresUserGesture() {
 }
 
 function removeBehaviorsRestrictions() {
+    "use strict";
     audio = document.getElementById('player');
     audio.load();
     window.removeEventListener('keydown', removeBehaviorsRestrictions);
@@ -194,17 +199,18 @@ function removeBehaviorsRestrictions() {
 }
 
 function setupSound() {
+    "use strict";
     if (requiresUserGesture()) {
         window.addEventListener('keydown', removeBehaviorsRestrictions);
         window.addEventListener('mousedown', removeBehaviorsRestrictions);
         window.addEventListener('touchstart', removeBehaviorsRestrictions);
-    }
-    else {
+    } else {
         setSource();
     }
 }
 
 function calibrateValues() {
+    "use strict";
     var aSin = 0;
     var bSin = 0;
     var gSin = 0;
@@ -219,7 +225,7 @@ function calibrateValues() {
         aCos += Math.cos(convert("rad", totalAlpha[i]));
         aSin += Math.sin(convert("rad", totalAlpha[i]));
     }
-    calibAlpha = (360+ convert("deg", Math.atan2(aSin, aCos)))%360;
+    calibAlpha = (360 + convert("deg", Math.atan2(aSin, aCos))) % 360;
 
     i = 0;
     for (i in totalBeta) {
@@ -235,15 +241,12 @@ function calibrateValues() {
     }
     calibGamma = convert("deg", Math.atan2(gSin, gCos));
 
-    message+="Calibrated!\n"
-}
-
-function createFile() {
-
+    message += "Calibrated!\n";
 }
 
 //Controller for app set first
 function balanceController($scope, $interval) {
+    "use strict";
     setValues();
     $scope.getStyle = function () {
         var s = "width:" + realWidth + "px; height:" + realHeight + "px; font-size:xx-large";
@@ -255,7 +258,7 @@ function balanceController($scope, $interval) {
     };
 
     $scope.nextScreen = function () {
-        screenNum++;
+        screenNum = screenNum + 1;
     };
 
     $scope.setScreen = function (num) {
@@ -296,23 +299,23 @@ function balanceController($scope, $interval) {
         Android.makeFile(message);
 
         //Reset variables
-         angleCalibrations = [0, 0, 0];
+        angleCalibrations = [0, 0, 0];
 
-         alpha = [];
-         beta = [];
-         gamma = [];
+        alpha = [];
+        beta = [];
+        gamma = [];
 
-         totalAlpha = [];
-         totalBeta = [];
-         totalGamma = [];
-         calibAlpha = 0;
-         calibBeta = 0;
-         calibGamma = 0;
-         diffAlpha = 0;
-         diffBeta = 0;
-         diffGamma = 0;
-         smoothedAlpha = 0;
-         smoothedBeta = 0;
+        totalAlpha = [];
+        totalBeta = [];
+        totalGamma = [];
+        calibAlpha = 0;
+        calibBeta = 0;
+        calibGamma = 0;
+        diffAlpha = 0;
+        diffBeta = 0;
+        diffGamma = 0;
+        smoothedAlpha = 0;
+        smoothedBeta = 0;
         smoothedGamma = 0;
 
         rawValues = [];
@@ -327,12 +330,12 @@ function balanceController($scope, $interval) {
     $scope.startCalib = function () {
         message = "";
         calibTime = 0;
+        orient();
         calib = $interval(function () {
-            calibTime+=0.5;
+            calibTime = calibTime + 1;
             document.getElementById('progress').style.width = Math.ceil((calibTime * 10) / 15) + "%";
             showTime();
-            orient();
-            if (calibTime % 5 === 0 && calibTime>0) {
+            if (calibTime % 5 === 0 && calibTime > 0) {
                 smoothAngles(true);
             }
             if (calibTime > 150) {
@@ -342,16 +345,15 @@ function balanceController($scope, $interval) {
                 $interval.cancel(calib);
                 $scope.startRunning();
             }
-        }, 50);
+        }, 100);
     };
 
     var running;
     $scope.startRunning = function () {
         var runTime = 0;
+        orient();
         running = $interval(function () {
-            //document.getElementById('testing').innerHTML = "Running: " + angular.isDefined(running);
-            orient();
-            runTime++;
+            runTime = runTime + 1;
             if (runTime % 5 === 0 && runTime > 0) {
                 smoothAngles(false);
                 runTime = 0;
@@ -359,8 +361,8 @@ function balanceController($scope, $interval) {
         }, 100);
     };
 
-    $scope.getTime = function(){
+    $scope.getTime = function () {
         var today = new Date();
         return today.toGMTString();
-    }
+    };
 }
