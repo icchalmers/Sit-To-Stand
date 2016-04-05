6a8bf3bcbba12e5c0ff425f7aa3aa8c4751be8c1
Iain Chalmers <iain@chalmers.cc>
2016-04-05 03:21:47 +0100

    Removing old redundant files

--- GyroScope/GyroScope/bld/Debug/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Craig\Documents\FinalYearProject\GyroScope\GyroScope\bld\Debug\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\Users\Craig\Documents\FinalYearProject\GyroScope\GyroScope\bld\Debug\platforms\android\CordovaLib\AndroidManifest.xml \
